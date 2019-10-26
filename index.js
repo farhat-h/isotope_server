b9339c9b7d9de1ec6afe5eb149fb47be89b94d36
farhat-h <farhathaythem1@gmail.com>
2019-10-26 17:54:37 +0100

    session parsing

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-
+const fetch = require("node-fetch");
 const Parser = require("./src/Parser");
+const formData = require("form-data");
+const { JSDOM } = require("jsdom");
+const Schedule = require("./src/Schedule");
+
+const config = {
+    baseUrl: "http://www.issatso.rnu.tn/fo/emplois/emploi_groupe.php"
+}
 
 const p = new Parser();
 
 p.initialize()
-    .then(async majors => console.log(majors)) 
+    .then(async function (majors) {
+        try {
+            const body = new formData();
+            body.append("jeton", p.token);
+            body.append("id", majors[0].id)
+
+            const params = {
+                method: "POST",
+                headers: new fetch.Headers([["Cookie", p.cookie]]),
+                body
+            }
+
+            const html = await fetch(config.baseUrl, params)
+                .then(res => res.text());
+
+            const { document: doc } = new JSDOM(html).window;
+            const s = new Schedule(doc, majors[0]).getSessions();
+        } catch (error) {
+            console.error(error);
+
+        }
+    }) 
--- src/Schedule.js
+++ src/Schedule.js
@@ -1,17 +1,51 @@
 
 class Schedule {
+    static r_subgroup = /(\d)$/;
+    static r_day = /^(\d)-(Lundi|Mardi|Mercredi|Jeudi|Vendredi|Samedi)$/;
     constructor(document, major) {
         this.document = document;
         this.major = major;
     }
     getSessions() {
         const isScheduleEmpty =
-            this.document.querySelector("#dvContainer tbody").innerHTML === "";
-        if (isScheduleEmpty) {
+            this.document.querySelector("#dvContainer tbody")
+                .innerHTML === "";
+        if (isScheduleEmpty)
             throw new Error(`Schedule for ${major.fullName} is empty`)
+
+        let sessions = Array.from(this.document.querySelectorAll("#dvContainer tbody tr"));
+        let subGroup = 1;
+        let day = 1;
+        for (let i = 0; i < sessions.length; i++) {
+            let sesh = sessions[i];
+
+            const [firstCell] = sesh.children;
+            if (
+                firstCell.textContent !== "" &&
+                firstCell.textContent.includes(this.major.fullName)
+            ) {
+                // SubGroup Row
+                const [_, subg] = Schedule.r_subgroup.exec(firstCell.textContent)
+                subGroup = subg;
+            }
+            else if (Schedule.r_day.test(firstCell.textContent)) {
+                const [_, d] = Schedule.r_day.exec(firstCell.textContent);
+                day = d
+            }
+            else if (sesh.innerHTML === "") {
+                continue;
+            } else {
+
+                const time = sesh.children[1].textContent;
+                const subject = sesh.children[4].textContent;
+                const professor = sesh.children[5].textContent;
+                const type = sesh.children[6].textContent;
+                const room = sesh.children[7].textContent;
+                const regime = sesh.children[8].textContent;
+                console.log({ subGroup, day, time, subject, professor, type, room, regime })
+            }
+
         }
-
-
     }
 }
 

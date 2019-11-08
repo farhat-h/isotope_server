364ca962aca118faeb2069069e47cd94952fe8b5
farhat-h <farhathaythem1@gmail.com>
2019-11-09 00:51:16 +0100

    schema update

--- database.js
+++ database.js
@@ -3,55 +3,72 @@ const path = require("path");
 const pathToDbs = path.resolve(__dirname, "dbs/");
 
 class Database {
-    constructor(databaseName = "db.sqlite") {
-        this.databaseName = databaseName.endsWith(".sqlite") ? databaseName : databaseName + ".sqlite";
-        const pathToDatabase = path.join(pathToDbs, this.databaseName);
+  constructor(databaseName = "db.sqlite") {
+    this.databaseName = databaseName.endsWith(".sqlite")
+      ? databaseName
+      : databaseName + ".sqlite";
+    const pathToDatabase = path.join(pathToDbs, this.databaseName);
 
-        this.sequelize = new Sequelize({
-            dialect: "sqlite",
-            logging: false,
-            storage: pathToDatabase.toString(),
-            define: {
-                timestamps: false
-            }
-        });
+    this.sequelize = new Sequelize({
+      dialect: "sqlite",
+      logging: false,
+      storage: pathToDatabase.toString(),
+      define: {
+        timestamps: false
+      }
+    });
 
+    this.Major = class Major extends Sequelize.Model {};
 
-        this.Major = class Major extends Sequelize.Model {
-        };
+    this.Session = class Session extends Sequelize.Model {};
 
-        this.Session = class Session extends Sequelize.Model {
-        };
+    this.Major.init(
+      {
+        majorId: {
+          type: Sequelize.STRING,
+          primaryKey: true,
+          allowNull: false
+        },
+        year: Sequelize.NUMBER,
+        group: Sequelize.STRING,
+        fullName: Sequelize.STRING,
+        majorName: Sequelize.STRING
+      },
+      {
+        sequelize: this.sequelize
+      }
+    );
 
-        this.Major.init({
-            majorId: {type: Sequelize.STRING, primaryKey: true},
-            year: Sequelize.NUMBER,
-            group: Sequelize.STRING,
-            fullName: Sequelize.STRING,
-            majorName: Sequelize.STRING
-        }, {sequelize: this.sequelize});
+    this.Session.init(
+      {
+        sessionId: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          allowNull: false,
+          autoIncrement: true
+        },
+        day: Sequelize.NUMBER,
+        time: Sequelize.STRING,
+        subject: Sequelize.STRING,
+        professor: Sequelize.STRING,
+        type: Sequelize.STRING,
+        room: Sequelize.STRING,
+        regime: Sequelize.STRING,
+        subGroup: Sequelize.NUMBER
+      },
+      { sequelize: this.sequelize }
+    );
 
-        this.Session.init({
-            day: Sequelize.NUMBER,
-            time: Sequelize.STRING,
-            subject: Sequelize.STRING,
-            professor: Sequelize.STRING,
-            type: Sequelize.STRING,
-            room: Sequelize.STRING,
-            regime: Sequelize.STRING,
-            subGroup: Sequelize.NUMBER,
-        }, {sequelize: this.sequelize});
+    this.Major.hasMany(this.Session, { foreignKey: "majorId" });
+  }
 
-        this.Major.hasMany(this.Session, {foreignKey: "majorId"});
-    }
+  initialize() {
+    return this.sequelize.sync();
+  }
 
-    initialize() {
-        return this.sequelize.sync();
-    }
-
-    destroy () {
-        return this.sequelize.close();
-    }
+  destroy() {
+    return this.sequelize.close();
+  }
 }
 
 module.exports = Database;
--- scraper/index.js
+++ scraper/index.js
@@ -1,4 +1,3 @@
-
 const EventEmitter = require("events");
 const request = require("request-promise-native");
 const Major = require("./Major");
@@ -9,136 +8,147 @@ const { getCurrentDatabaseVersion, setDbVersion } = require("../dbs");
 
 const URL = "http://www.issatso.rnu.tn/fo/emplois/emploi_groupe.php";
 class Crawler extends EventEmitter {
+  constructor() {
+    super();
+    this.document = null;
+    this.majors = null;
+    this.token = null;
+    this.cookie = null;
+    this.databaseName = null;
+    this.retryQueue = [];
 
-    constructor() {
-        super();
-        this.document = null;
-        this.majors = null;
-        this.token = null;
-        this.cookie = null;
-        this.databaseName = null;
-        this.retryQueue = [];
+    this.on("setup-db", this._setupDB);
+    this.on("begin-parsing", this._parse);
+    this.on("insert-data", this._onInsertData);
+    this.on("no-body-error", this._onError);
+    this.on("start-retry-queue", this._startRetryQueue);
+    this.on("done", this._onDone);
+    this.init();
+  }
+  async init() {
+    console.time("download-time");
+    const res = await request(URL, {
+      method: "GET",
+      resolveWithFullResponse: true
+    });
+    const _cookie = res.headers["set-cookie"][0].split(";")[0];
+    this.cookie = _cookie;
 
-        this.on("setup-db", this._setupDB);
-        this.on("begin-parsing", this._parse);
-        this.on("insert-data", this._onInsertData)
-        this.on("no-body-error", this._onError);
-        this.on("start-retry-queue", this._startRetryQueue);
-        this.on("done", this._onDone);
-        this.init();
-    }
-    async init() {
-        console.time("download-time")
-        const res = await request(URL, {
-            method: "GET",
-            resolveWithFullResponse: true,
-        });
-        const _cookie = res.headers['set-cookie'][0].split(";")[0];
-        this.cookie = _cookie;
+    const _document = new JSDOM(res.body).window.document;
+    this.document = _document;
 
-        const _document = new JSDOM(res.body).window.document;
-        this.document = _document;
+    const _databaseName =
+      /:\s(.*)$/
+        .exec(
+          this.document.querySelector(".content.container center h5")
+            .textContent
+        )[1]
+        .trim() + ".sqlite";
 
-        const _databaseName = /:\s(.*)$/.exec(this.document.querySelector(".content.container center h5").textContent)[1].trim() + ".sqlite";
+    const _token = this.document.querySelector("input[type=hidden][name=jeton]")
+      .value;
+    this.token = _token;
 
-        const _token = this.document.querySelector("input[type=hidden][name=jeton]").value;
-        this.token = _token;
-
-
-        this.emit("setup-db", _databaseName)
-    }
-    _onError(major) {
-        console.log(major.fullName + "has no schedule yet")
-    }
-    async _onInsertData(data, collection) {
-        switch (collection) {
-            case "majors":
-                await this.database.Major.bulkCreate(data.map(m => m.serialized()));
-                console.log("MAJORS INSERTED")
-                break;
-            case "sessions":
-                await this.database.Session.bulkCreate(data);
-                break;
-            default:
-                break;
-        }
-
-    }
-
-    async _parse() {
-        this.majors =
-            Array.from(this.document.querySelectorAll("select[name=id] option"))
-                .map(item => new Major(item));
-
-        this.emit("insert-data", this.majors, "majors")
-
-        await Promise.all(this.majors.map(m => this._parseMajorSchedule(m)));
-        this.emit("start-retry-queue");
-    }
-    async _parseMajorSchedule(major) {
-
-        const body = {
-            jeton: this.token,
-            id: major.id
-        }
-        let r = null
+    this.emit("setup-db", _databaseName);
+  }
+  _onError(major) {
+    console.log(major.fullName + "has no schedule yet");
+  }
+  async _onInsertData(data, collection) {
+    switch (collection) {
+      case "majors":
         try {
-            r = await request.post(URL, {
-                timeout: 30 * 1000,
-                headers: { 'Cookie': this.cookie },
-                form: body
-            });
+          await this.database.Major.bulkCreate(data.map(m => m.serialized()));
+          console.log("MAJORS INSERTED");
         } catch (error) {
-            console.log("error requesting schedule for " + major.fullName);
+          console.error(error.message);
+          process.exit(1);
         }
-        if (r === null) {
-            this.retryQueue.push(major)
-            return
-        };
-        const { document } = new JSDOM(r).window;
-        let s = null
+
+        break;
+      case "sessions":
         try {
-            s = new Schedule(document, major).getSessions();
+          await this.database.Session.bulkCreate(data);
         } catch (error) {
-            if (error.message === "no-schedule-error")
-                this.emit("no-body-error", major);
-            else console.log(error)
+          console.error(error);
+          process.exit(1);
         }
-        if (s !== null) {
-            this.emit("insert-data", s, "sessions")
-            console.log("downloaded " + major.fullName);
-        }
+        break;
+      default:
+        break;
+    }
+  }
 
-    }
-    async _setupDB(dbName) {
+  async _parse() {
+    this.majors = Array.from(
+      this.document.querySelectorAll("select[name=id] option")
+    ).map(item => new Major(item));
 
-        const lastDbVersion = getCurrentDatabaseVersion();
-        if (dbName === lastDbVersion) {
-            console.log("DATABASE ALREADY AQUIRED\n exiting.")
-            process.exit(0);
-        } else {
-            console.log("CREATING A NEW DATABASE: " + dbName)
-            this.databaseName = dbName;
-            this.database = new Database(this.databaseName);
-            await this.database.initialize();
-            this.emit("begin-parsing");
-        }
+    this.emit("insert-data", this.majors, "majors");
+
+    await Promise.all(this.majors.map(m => this._parseMajorSchedule(m)));
+    this.emit("start-retry-queue");
+  }
+  async _parseMajorSchedule(major) {
+    const body = {
+      jeton: this.token,
+      id: major.id
+    };
+    let r = null;
+    try {
+      r = await request.post(URL, {
+        timeout: 30 * 1000,
+        headers: { Cookie: this.cookie },
+        form: body
+      });
+    } catch (error) {
+      console.log("error requesting schedule for " + major.fullName);
     }
-    _onDone() {
-        setDbVersion(this.databaseName);
-        console.timeEnd("download-time")
+    if (r === null) {
+      this.retryQueue.push(major);
+      return;
     }
-    async _startRetryQueue() {
-        console.log("starting sequential queue for failed attempts")
-        if (this.retryQueue.length != 0) {
-            for (let i = 0; i < this.retryQueue.length; i++) {
-                const major = this.retryQueue[i];
-                await this._parseMajorSchedule(major);
-            }
-        }
-        this.emit("done")
+    const { document } = new JSDOM(r).window;
+    let s = null;
+    try {
+      s = new Schedule(document, major).getSessions();
+    } catch (error) {
+      if (error.message === "no-schedule-error")
+        this.emit("no-body-error", major);
+      else console.log(error);
     }
+    if (s !== null) {
+      this.emit("insert-data", s, "sessions");
+      console.log("downloaded " + major.fullName);
+    }
+  }
+  async _setupDB(dbName) {
+    const lastDbVersion = getCurrentDatabaseVersion();
+    if (dbName === lastDbVersion) {
+      console.log("DATABASE ALREADY AQUIRED\n exiting.");
+      process.exit(0);
+    } else {
+      console.log("CREATING A NEW DATABASE: " + dbName);
+      this.databaseName = dbName;
+      this.database = new Database(this.databaseName);
+      await this.database.initialize();
+      this.emit("begin-parsing");
+    }
+  }
+  _onDone() {
+    setDbVersion(this.databaseName);
+    console.timeEnd("download-time");
+  }
+  async _startRetryQueue() {
+    console.log("starting sequential queue for failed attempts");
+    if (this.retryQueue.length != 0) {
+      for (let i = 0; i < this.retryQueue.length; i++) {
+        const major = this.retryQueue[i];
+        await this._parseMajorSchedule(major);
+      }
+    }
+    this.emit("done");
+  }
 }
-
 
 new Crawler();
